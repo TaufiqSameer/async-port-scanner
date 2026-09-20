2d12330b60a20366123fffa1035c72881b63174c
NemesisX-ai <sameertanker@gmail.com>
2026-09-20 15:54:25 +0530

    added concurrency and semaphores

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ use std::net::{Ipv4Addr, SocketAddr};
 use std::time::{Duration, Instant};
 use tokio::net::{TcpStream};
 use tokio::time::{timeout};
+use tokio::sync::{Semaphore};
+use std::sync::{Arc};
 
 enum PortStatus{
     Open,
     Closed,
-    TimedOut
+    TimedOut,
+    SemaphoretimedOut
 }
 
 #[tokio::main]
@@ -19,13 +22,54 @@ async fn main() {
     let _port_to_be_scanned = 3000;
     let start = Instant::now();  
     let ip = Ipv4Addr::new(127, 0, 0, 1);
+    let semaphore = Arc::new(Semaphore::new(50));
+    let mut handles = Vec::new();
     for port_number in start_port..=end_port {
         println!("Scanning port {}", port_number);
-        let res = port_scan(ip, port_number).await;
+        let semaphore = Arc::clone(&semaphore);
+        let res = tokio::spawn(async move {
+            let permit = semaphore.acquire_owned().await;
+            match permit {
+                Ok(_) => {
+                    let scan = port_scan(ip, port_number).await;
+                    scan
+                }
+                Err(_) => {
+                    PortStatus::SemaphoretimedOut
+                }
+            }
+     
+        });
+        handles.push((port_number,res));
+        // match res {
+        //     PortStatus::Closed => println!("The port {} is closed",port_number),
+        //     PortStatus::Open => println!("The port {} is open",port_number),
+        //     PortStatus::TimedOut => println!("The port {} timed out",port_number)
+        // }
+    }
+    for (port,h) in handles{
+        let res = h.await;
+
         match res {
-            PortStatus::Closed => println!("The port {} is closed",port_number),
-            PortStatus::Open => println!("The port {} is open",port_number),
-            PortStatus::TimedOut => println!("The port {} timed out",port_number)
+            Ok(status ) => {
+                match status {
+                    PortStatus::Open => {
+                        println!("The port is open {}",port);
+                    }
+                    PortStatus::Closed => {
+                        println!("The port is closed {}",port);
+                    }
+                    PortStatus::TimedOut => {
+                        println!("The port is timedout {}",port);
+                    }
+                    PortStatus::SemaphoretimedOut => {
+                        println!("This should not happen here");
+                    }
+                }
+            },
+            Err(e) => {
+                println!("Error {}",e);
+            }
         }
     }
     let end = Instant::now();
